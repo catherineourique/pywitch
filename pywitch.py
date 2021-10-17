9710016407e420cfbb10b4f8cd038198922c2b24
Gustavo <ourique.gustavo@gmail.com>
2021-10-16 23:32:51 -0300

    Added tmi websocket routine

--- pywitch.py
+++ pywitch.py
@@ -1,10 +1,10 @@
 import asyncio
-import pytmi
 import time
 import websockets
 import json
 import random
 import requests
+import threading
 
 __validate_token_url__ = 'https://id.twitch.tv/oauth2/validate'
 
@@ -56,11 +56,33 @@ def nonce(length):
     return text
 
 
+def get_display_name(string):
+    try:
+        ipos = string.find('display-name') + 13
+        fpos = string.find(';', ipos)
+        return string[ipos:fpos]
+    except:
+        return ''
+
+
+def get_privmsg(string):
+    try:
+        ipos = string.find('PRIVMSG')
+        ipos = string.find(':', ipos) + 1
+        fpos = string.find('\r\n', ipos)
+        return string[ipos:fpos]
+    except:
+        return ''
+
+
 class PyWitch:
     def __init__(self, channel, token=None, verbose=1):
         self.channel = channel
         self.token = token
         self.verbose = verbose
+        self.threads = {}
+        self.data = {}
+        self.thread_kind = {'tmi': self.thread_tmi}
 
         if not (self.token):
             self.log(no_token_msg)
@@ -72,10 +94,62 @@ class PyWitch:
         response = requests.get(__validate_token_url__, headers=headers)
         if response.status_code == 200:
             self.validation = response.json()
-            self.log(" Successfully validated token!")
+            self.log("Successfully validated token!")
         else:
             self.log(f"Failed to validate token: {response.json()}")
             raise Exception('Failed to validate token')
+
+    def start_thread(self, kind):
+        if not kind in self.thread_kind:
+            self.log(f"Invalid thread kind! Returning")
+            return False
+        thread = threading.Thread(target=self.start_async, args=(kind,))
+        self.threads[kind] = {'thread': thread, 'running': True}
+        self.threads[kind]['thread'].start()
+        return thread
+
+    def stop_signal_thread(self, kind):
+        if not kind in self.threads:
+            self.log(f"Invalid thread kind! Returning")
+            return False
+        self.threads[kind]['running'] = False
+        self.threads[kind]['thread'].join()
+        self.threads.pop(kind)
+
+    def stop_signal_all_threads(self):
+        kinds = list(self.threads.keys())
+        for kind in kinds:
+            self.stop_signal_thread(kind)
+
+    def start_async(self, kind):
+        while self.threads[kind]['running']:
+            self.thread_kind[kind]
+            asyncio.run(self.thread_kind[kind]())
+
+    async def thread_tmi(self):
+        try:
+            kind = 'tmi'
+            async with websockets.connect(
+                "wss://irc-ws.chat.twitch.tv:443"
+            ) as websocket:
+                cap = 'twitch.tv/tags twitch.tv/commands twitch.tv/membership'
+                await websocket.send(f'CAP REQ : {cap}')
+                await websocket.send(f'PASS oauth:{self.token}')
+                await websocket.send(f'NICK {self.channel}')
+                await websocket.send(f'JOIN #{self.channel}')
+                while self.threads[kind]['running']:
+                    event = await websocket.recv()
+                    event_time = time.time()
+                    display_name = get_display_name(event)
+                    message = get_privmsg(event)
+                    self.data[kind] = {
+                        'display_name': display_name,
+                        'event_time': event_time,
+                        'message': message,
+                        'event': event,
+                    }
+        except:
+            return
 
     def log(self, msg, level=1):
         if self.verbose >= level:
